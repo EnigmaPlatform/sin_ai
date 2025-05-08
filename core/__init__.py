39335759d97634989f956f59314fccfbe95d6f21
Millennium <71434392+EnigmaPlatform@users.noreply.github.com>
2025-05-08 17:59:37 +1100

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,16 @@
+"""
+Основные модули Sin AI
+"""
+
+from .network import SinNetwork
+from .memory import MemorySystem
+from .learning import LearningEngine
+from .api_handler import DeepSeekAPIHandler
+from .code_analyzer import CodeAnalyzer
+from .sandbox import CodeSandbox
+from .level_system import LevelSystem
+from .monitoring import start_monitoring, monitor
+from .plugins import PluginManager
+from .personality import PersonalityCore
 from .emotions import EmotionEngine
+from .deepseek_trainer import DeepSeekTrainer
