62575fdec99269b5fb93aaf78457a688a209f974
Millennium <71434392+EnigmaPlatform@users.noreply.github.com>
2025-05-09 22:05:53 +1100

    Update base.py

--- plugins/base.py
+++ plugins/base.py
@@ -5,13 +5,10 @@ if TYPE_CHECKING:
 
 class SinPlugin:
     def initialize(self, network: 'SinNetwork') -> None:
-        """Инициализация плагина с доступом к основной сети"""
         self.network = network
     
     def get_commands(self) -> Dict[str, str]:
-        """Возвращает словарь команд плагина {команда: описание}"""
         raise NotImplementedError
     
     def execute_command(self, command: str, args: str) -> Any:
-        """Выполнение команды плагина"""
         raise NotImplementedError
