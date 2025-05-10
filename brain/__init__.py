83c929364c861862c3b6dd697ddbb92eb046cb1d
Millennium <71434392+EnigmaPlatform@users.noreply.github.com>
2025-05-10 22:29:42 +1100

    Update __init__.py

--- brain/__init__.py
+++ brain/__init__.py
@@ -1,5 +1,7 @@
 from .model import SinModel
 from .memory import SinMemory
 from .trainer import SinTrainer
+import logging
+logger = logging.getLogger(__name__)
 
 __all__ = ['SinModel', 'SinMemory', 'SinTrainer']
