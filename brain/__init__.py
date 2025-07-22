c68d14a2c0018d7aff43038e18efcbc1a189b3a2
Millennium <71434392+EnigmaPlatform@users.noreply.github.com>
2025-07-22 12:38:34 +1100

    Delete brain directory

--- brain/__init__.py
+++
@@ -1,7 +0,0 @@
-from .model import SinModel
-from .memory import SinMemory
-from .trainer import SinTrainer, DialogDataset
-import logging
-logger = logging.getLogger(__name__)
-
-__all__ = ['SinModel', 'SinMemory', 'SinTrainer']
